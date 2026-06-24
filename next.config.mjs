23a9c3aac7157b0aebb272edbabaa44c912efdb6
muhammaddgaberr <muhammaddgaberr@gmail.com>
2026-06-25 01:24:52 +0300

    puplish

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Khaled_Portofolio',
+  assetPrefix: '/Khaled_Portofolio',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '/Khaled_Portofolio',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
